7bf718774ded70aa99bf5ae5c2297d5a2a8de5d9
awalias <antwilson@hotmail.co.uk>
2020-08-06 16:47:33 +0800

    fix: use goroutine to trigger restart after response has been made to caller

--- api/service.go
+++ api/service.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"time"
 )
 
 // RestartServices is the endpoint for fetching current goauth email config
@@ -23,28 +24,33 @@ func (a *API) RestartServices(w http.ResponseWriter, r *http.Request) error {
 
 	fmt.Fprintf(os.Stdout, string(stdout))
 
-	sudo = "sudo"
-	app = "systemctl"
-	arg0 = "restart"
-	arg1 := "services.slice"
+	// need to do command as goroutine because adminapi gets killed and can't respond
+	go func() {
+		sudo := "sudo"
+		app := "systemctl"
+		arg0 := "restart"
+		arg1 := "services.slice"
 
-	cmd = exec.Command(sudo, app, arg0, arg1)
-	stdout, err = cmd.Output()
+		// give time for http response
+		time.Sleep(2 * time.Second)
 
-	if err != nil {
-		fmt.Fprintf(os.Stderr, err.Error())
-		return sendJSON(w, http.StatusInternalServerError, err.Error())
-	}
+		cmd = exec.Command(sudo, app, arg0, arg1)
+		stdout, err = cmd.Output()
 
-	fmt.Fprintf(os.Stdout, string(stdout))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, err.Error())
+		}
+
+		fmt.Fprintf(os.Stdout, string(stdout))
+	}()
 
 	return sendJSON(w, http.StatusOK, 200)
 }
 
 // RebootMachine is the endpoint for fetching current goauth email config
 func (a *API) RebootMachine(w http.ResponseWriter, r *http.Request) error {
-	app := "reboot"
-	exec.Command(app)
+	// app := "reboot"
+	// exec.Command(app)
 
-	return sendJSON(w, http.StatusOK, 200)
+	return sendJSON(w, http.StatusInternalServerError, "endpoint not yet implemented")
 }
