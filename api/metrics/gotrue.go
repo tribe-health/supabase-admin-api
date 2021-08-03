6528a356a740ec0ad9cb7495e1f93ee94d79c652
Div Arora <root@darora.com>
2021-08-03 11:32:31 +0800

    feat: monitor gotrue and postgrest

---
+++ api/metrics/gotrue.go
@@ -0,0 +1,36 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"net/http"
+	"time"
+)
+
+type GotrueCollector struct {
+	up *prometheus.Desc
+	client *http.Client
+	url string
+}
+
+func NewGotrueCollector(gotrueUrl string) *GotrueCollector {
+	return &GotrueCollector{
+		up: prometheus.NewDesc("gotrue_up", "GoTrue status", nil, nil),
+		client: &http.Client{
+			Timeout: 500 * time.Millisecond,
+		},
+		url: gotrueUrl,
+	}
+}
+
+func (c *GotrueCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.up
+}
+
+func (c *GotrueCollector) Collect(ch chan<- prometheus.Metric) {
+	resp, err := c.client.Get(c.url)
+	status := float64(0)
+	if err != nil && resp != nil && resp.StatusCode == 200 {
+		status = float64(1)
+	}
+	ch <- prometheus.MustNewConstMetric(c.up, prometheus.GaugeValue, status)
+}
