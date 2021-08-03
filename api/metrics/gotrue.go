cc52bbfc52cde02e57fa52d767e49c1d08e16f8e
Div Arora <root@darora.com>
2021-08-03 12:42:29 +0800

    fix: success conditions

--- api/metrics/gotrue.go
+++ api/metrics/gotrue.go
@@ -29,7 +29,7 @@ func (c *GotrueCollector) Describe(ch chan<- *prometheus.Desc) {
 func (c *GotrueCollector) Collect(ch chan<- prometheus.Metric) {
 	resp, err := c.client.Get(c.url)
 	status := float64(0)
-	if err != nil && resp != nil && resp.StatusCode == 200 {
+	if err == nil && resp != nil && resp.StatusCode == 200 {
 		status = float64(1)
 	}
 	ch <- prometheus.MustNewConstMetric(c.up, prometheus.GaugeValue, status)
--- api/metrics/postgrest.go
+++ api/metrics/postgrest.go
@@ -29,7 +29,7 @@ func (c *PostgrestCollector) Describe(ch chan<- *prometheus.Desc) {
 func (c *PostgrestCollector) Collect(ch chan<- prometheus.Metric) {
 	resp, err := c.client.Get(c.url)
 	status := float64(0)
-	if err != nil && resp != nil && resp.StatusCode == 200 {
+	if err == nil && resp != nil && resp.StatusCode == 200 {
 		status = float64(1)
 	}
 	ch <- prometheus.MustNewConstMetric(c.up, prometheus.GaugeValue, status)
