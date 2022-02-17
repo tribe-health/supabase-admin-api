912df883e0249aed90c8c79092e90df1aafc45cd
dragarcia <angelico.delosreyes@gmail.com>
2022-02-17 13:26:01 +0800

    added configuration when performing a WAL-G restoration

--- api/walg_restore.go
+++ api/walg_restore.go
@@ -1,15 +1,35 @@
 package api
 
 import (
+	"encoding/json"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"net/http"
 	"os/exec"
 )
 
+// FileContents holds the content of a config file
+type RestoreConfiguration struct {
+	BackupName     string `json:"backup_name"`
+	RecoveryTargetTime string   `json:"recovery_target_time"`
+}
+
 func RestoreDatabase(w http.ResponseWriter, r *http.Request) error {
-	cmd := exec.Command("/bin/sh", "-c", "sudo /root/commence_walg_restore.sh")
+	params := &RestoreConfiguration{}
+
+	jsonDecoder := json.NewDecoder(r.Body)
+	if err := jsonDecoder.Decode(params); err != nil {
+		return sendJSON(w, http.StatusInternalServerError, err.Error())
+	}
+
+	var cmd *Cmd
+	if params.RecoveryTargetTime {
+		cmd = exec.Command("/bin/sh", "-c", "sudo /root/commence_walg_restore.sh", params.BackupName, params.RecoveryTargetTime)
+	} else{
+		cmd = exec.Command("/bin/sh", "-c", "sudo /root/commence_walg_restore.sh", params.BackupName)
+	}
 	output, err := cmd.Output()
+	
 	if err != nil {
 		return errors.Wrap(err, "failed to execute WAL-G restoration")
 	}
