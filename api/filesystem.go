91d219adf72c2c486fc54192e6f328426d22426c
Paul Cioanca <paul@cioan.ca>
2022-07-22 11:48:47 +0300

    feat: add support to allow expanding  multiple volumes (#89)
    * feat: add support to allow expanding  multiple volumes

--- api/filesystem.go
+++ api/filesystem.go
@@ -1,14 +1,40 @@
 package api
 
 import (
-	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"os/exec"
+
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
+type VolumeType = string
+
+const (
+	RootVolumeType VolumeType = "root"
+	DataVolumeType VolumeType = "data"
+)
+
+type ExpandFileSystemConfiguration struct {
+	VolumeType string `json:"volume_type"`
+}
+
 func ExpandFilesystem(w http.ResponseWriter, r *http.Request) error {
-	cmd := exec.Command("/bin/sh", "-c", "sudo /root/grow_fs.sh")
+	params := &ExpandFileSystemConfiguration{}
+
+	jsonDecoder := json.NewDecoder(r.Body)
+	if err := jsonDecoder.Decode(params); err != nil {
+		return sendJSON(w, http.StatusInternalServerError, err.Error())
+	}
+
+	if params.VolumeType != DataVolumeType && params.VolumeType != RootVolumeType {
+		return sendJSON(w, http.StatusBadRequest, "Invalid value provided for `volume_type`.")
+	}
+
+	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("sudo /root/grow_fs.sh %s", params.VolumeType))
+
 	output, err := cmd.Output()
 	if err != nil {
 		return errors.Wrap(err, "couldn't grow partition")
