ee4e28926fc4d80ac30af701cc810c11192f1dff
Paul Cioanca <paul.cioanca@supabase.io>
2022-08-04 06:48:56 +0300

    fix: provide password during call

--- api/database.go
+++ api/database.go
@@ -1,12 +1,18 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"os/exec"
 
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
+
+type UpgradeDatabaseConfiguration struct {
+	Password string `json:"password"`
+}
 
 func (a *API) PrepareDatabaseUpgrade(w http.ResponseWriter, r *http.Request) error {
 	cmd := exec.Command("/bin/sh", "-c", "sudo /root/prepare_pg_upgrade.sh")
@@ -20,12 +26,19 @@ func (a *API) PrepareDatabaseUpgrade(w http.ResponseWriter, r *http.Request) err
 }
 
 func (a *API) InitiateDatabaseUpgrade(w http.ResponseWriter, r *http.Request) error {
-	cmd := exec.Command("/bin/sh", "-c", "sudo /root/initiate_pg_upgrade.sh")
+	params := &UpgradeDatabaseConfiguration{}
+
+	jsonDecoder := json.NewDecoder(r.Body)
+	if err := jsonDecoder.Decode(params); err != nil {
+		return sendJSON(w, http.StatusInternalServerError, err.Error())
+	}
+
+	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("sudo /root/initiate_pg_upgrade.sh %s", params.Password))
 	output, err := cmd.Output()
 	if err != nil {
 		return errors.Wrap(err, "couldn't run pg_upgrade")
 	}
-	logrus.WithField("output", string(output)).Info("pg_upgrade run finished")
+	logrus.WithField("output", string(output)).Info("Finished running pg_upgrade")
 
 	return nil
 }
