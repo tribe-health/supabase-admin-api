9ba6f614739f3b8c77283025423ef618ec90b89d
Paul Cioanca <paul.cioanca@supabase.io>
2022-09-08 00:15:23 +0300

    feat: add major version param

--- api/database.go
+++ api/database.go
@@ -11,7 +11,8 @@ import (
 )
 
 type UpgradeDatabaseConfiguration struct {
-	Password string `json:"password"`
+	Password     string `json:"password"`
+	MajorVersion int    `json:"major_version"`
 }
 
 func (a *API) PrepareDatabaseUpgrade(w http.ResponseWriter, r *http.Request) error {
@@ -33,9 +34,10 @@ func (a *API) InitiateDatabaseUpgrade(w http.ResponseWriter, r *http.Request) er
 		return sendJSON(w, http.StatusInternalServerError, err.Error())
 	}
 
-	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("sudo /root/initiate_pg_upgrade.sh %s", params.Password))
+	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("sudo /root/initiate_pg_upgrade.sh %d %s", params.MajorVersion, params.Password))
 	output, err := cmd.Output()
 	if err != nil {
+		logrus.WithField("output", string(output)).Info("errored")
 		return errors.Wrap(err, "couldn't run pg_upgrade")
 	}
 	logrus.WithField("output", string(output)).Info("Finished running pg_upgrade")
